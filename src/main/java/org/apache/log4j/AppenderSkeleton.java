7796b635598291935d419f98ca4cfac6b9978e20
vinzlercodes <vinayak.sengupta@gmail.com>
2021-04-12 12:50:28 -0400

    TODO: finalize() method should be made protected not public

--- src/main/java/org/apache/log4j/AppenderSkeleton.java
+++ src/main/java/org/apache/log4j/AppenderSkeleton.java
@@ -133,7 +133,7 @@ public abstract class AppenderSkeleton implements Appender, OptionHandler {
      <code>close</code> method.
 
      @since 0.8.4 */
-  //TODO: finalize() should be made protected not public
+  //TODO: finalize() method should be made protected not public
   public
   void finalize() {
     // An appender might be closed then garbage collected. There is no
